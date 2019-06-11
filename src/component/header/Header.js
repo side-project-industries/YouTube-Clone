9e49e4b7b5f43db8057c6425b03b643a0f8460cb
Poly Goveia <poly.goveia@gmail.com>
2019-06-11 14:06:54 -0700

    add font awesome

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
       <div>
         <h1>Header</h1>
+        <i class="logo fab fa-youtube fa-3x" />
       </div>
     );
   }
