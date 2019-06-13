bf9ef67de7458f954d22aa5efd5aa735dfd7f792
Kay Jay <kharris2k16@gmail.com>
2019-06-12 20:21:58 -0700

    WIP styles in place for sidenav component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Header from "./component/header/Header";
+import Sidenav from "./component/sidenav/Sidenav";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Sidenav />
       </div>
     );
   }
--- src/component/sidenav/Sidenav.js
+++ src/component/sidenav/Sidenav.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
+import "./Sidenav.css";
 
 class Sidenav extends Component {
   render() {
     return (
       <div className="sidenav-container">
-        <ul>
-          <li className="sidenav-button">Home</li>
-          <li className="sidenav-button">Trending</li>
-          <li className="sidenav-button">Subscriptions</li>
+        <ul className="sidenav-list">
+          <li className="sidenav-button"><i className="fas fa-lg fa-home"></i> Home</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-fire"></i> Trending</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-envelope-square"></i> Subscriptions</li>
           <br/>
-          <li className="sidenav-button">Library</li>
-          <li className="sidenav-button">History</li>
-          <li className="sidenav-button">Watch Later</li>
-          <li className="sidenav-button">Liked Videos</li>
-          <li className="sidenav-button">Favorites</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-folder"></i> Library</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-history"></i> History</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-clock"></i> Watch Later</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-thumbs-up"></i> Liked Videos</li>
+          <li className="sidenav-button"><i className="fas fa-lg fa-heart"></i> Favorites</li>
           <br/>
           <h4>Subscriptions</h4>
           <li className="sidenav-button">{this.props.dataIHaventMadeYet}</li>
